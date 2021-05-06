1a2bfdd1f554e4a307316314c71beb55e5f46461
gurhank <gurhankocyigit@gmail.com>
2021-05-05 19:46:17 -0500

    BasicNavigations class

--- src/test/java/com/cybertek/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cybertek/tests/day1_selenium_intro/BasicNavigations.java
@@ -25,33 +25,38 @@ public class BasicNavigations {
         System.out.println("currentUrl = " + currentUrl);
 
         //putting 3 seconds of wait/ stops the code for 3 sec
-        Thread.sleep(3000);
+        Thread.sleep(2000);
 
         //Going back using navigations
         driver.navigate().back();
 
         //putting 3 seconds of wait/ stops the code for 3 sec
-        Thread.sleep(3000);
+        Thread.sleep(2000);
 
         //Going forward using navigations
         driver.navigate().forward();
 
         //putting 3 seconds of wait/ stops the code for 3 sec
-        Thread.sleep(3000);
+        Thread.sleep(2000);
 
         //refreshing the page using navigations
         driver.navigate().refresh();
 
         //putting 3 seconds of wait/ stops the code for 3 sec
-        Thread.sleep(3000);
+        Thread.sleep(2000);
 
         //going to another url using .to() method
         driver.navigate().to("https://www.google.com");
 
         System.out.println("Current title: "+driver.getTitle());
 
+        currentUrl = driver.getCurrentUrl();
+
+        System.out.println("currentUrl = " + currentUrl);
 
 
+        //this line will basically maximize the browser size
+        driver.manage().window().maximize();
 
 
     }
