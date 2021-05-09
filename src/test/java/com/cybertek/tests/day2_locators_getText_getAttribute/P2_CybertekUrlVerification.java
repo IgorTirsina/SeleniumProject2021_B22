9192efbbf33b68738106b240c208c4a22fa83bd4
gurhank <gurhankocyigit@gmail.com>
2021-05-09 10:07:46 -0500

    day 2 practice 2

---
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P2_CybertekUrlVerification.java
@@ -0,0 +1,17 @@
+package com.cybertek.tests.day2_locators_getText_getAttribute;
+
+public class P2_CybertekUrlVerification {
+
+    public static void main(String[] args) {
+
+        //TC #2: Cybertek practice tool verifications
+        //1. Open Chrome browser
+        //2. Go to https://practice.cybertekschool.com
+        //3. VerifyURLcontains
+        //Expected: cybertekschool
+        //4. Verify title:
+        //Expected: Practice
+
+    }
+
+}
