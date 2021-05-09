e22735c258556db53175a5b84aac992422338232
gurhank <gurhankocyigit@gmail.com>
2021-05-09 15:36:22 -0500

    day 2 practice 8 ZeroBank  verifications

---
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/T2_ZeroBankAttributeVerification.java
@@ -0,0 +1,18 @@
+package com.cybertek.tests.day2_locators_getText_getAttribute;
+
+public class T2_ZeroBankAttributeVerification {
+
+    public static void main(String[] args) {
+
+        //TC #2: Zero Bank link text verification
+        //1. Open Chrome browser
+        //2. Go to http://zero.webappsecurity.com/login.html
+        // 3. Verify link text from top left:
+        //Expected: “Zero Bank”
+        //4. Verify link href attribute value contains:
+        //Expected: “index.html”
+
+
+    }
+
+}
