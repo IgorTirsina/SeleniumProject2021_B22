5de0c54d6a483fb853c2307fe69a1622ec97b5ba
gurhank <gurhankocyigit@gmail.com>
2021-05-09 11:15:16 -0500

    day 2 practice 3 click to link

--- src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P3_NavigationAndTitleVerification.java
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P3_NavigationAndTitleVerification.java
@@ -1,6 +1,7 @@
 package com.cybertek.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -18,6 +19,8 @@ public class P3_NavigationAndTitleVerification {
         driver.get("https:/www.google.com");
 
         //3- Click to Gmail from top right.
+        driver.findElement(By.linkText("Gmail")).click();
+
         //4- Verify title contains:
         //Expected: Gmail
         //5- Go back to Google by using the .back();
