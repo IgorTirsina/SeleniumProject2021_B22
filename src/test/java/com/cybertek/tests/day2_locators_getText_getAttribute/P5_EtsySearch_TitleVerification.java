2e414d34be6af983cde617c306e7ccc8f36ba328
gurhank <gurhankocyigit@gmail.com>
2021-05-09 13:23:04 -0500

    day 2 practice 3 click to link

---
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P5_EtsySearch_TitleVerification.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests.day2_locators_getText_getAttribute;
+
+public class P5_EtsySearch_TitleVerification {
+
+    public static void main(String[] args) {
+
+        //TC #1: Etsy Title Verification
+        // 1. Open Chrome browser
+        // 2. Go to https://www.etsy.com
+        // 3. Search for “wooden spoon”
+        // 4. Verify title:
+        //Expected: “Wooden spoon | Etsy”
+
+
+    }
+}
