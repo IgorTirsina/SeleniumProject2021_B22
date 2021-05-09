230db231497aca239271092125f8ade27c0e1e3b
gurhank <gurhankocyigit@gmail.com>
2021-05-09 13:24:29 -0500

    day 2 practice 5 ETSY

--- src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P5_EtsySearch_TitleVerification.java
+++ src/test/java/com/cybertek/tests/day2_locators_getText_getAttribute/P5_EtsySearch_TitleVerification.java
@@ -1,11 +1,19 @@
 package com.cybertek.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class P5_EtsySearch_TitleVerification {
 
     public static void main(String[] args) {
 
         //TC #1: Etsy Title Verification
         // 1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
         // 2. Go to https://www.etsy.com
         // 3. Search for “wooden spoon”
         // 4. Verify title:
