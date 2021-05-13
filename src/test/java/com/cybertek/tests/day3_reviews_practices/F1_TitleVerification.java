895959bcaedead1c471f958bcbc376227f214ade
gurhank <gurhankocyigit@gmail.com>
2021-05-12 19:34:43 -0500

    day 3 Facebook task 1

--- src/test/java/com/cybertek/tests/day3_reviews_practices/F1_TitleVerification.java
+++ src/test/java/com/cybertek/tests/day3_reviews_practices/F1_TitleVerification.java
@@ -21,10 +21,22 @@ public class F1_TitleVerification {
         driver.manage().window().maximize();
 
         // 2. Go to https://www.facebook.com
+        driver.get("https://www.facebook.com");
 
 
         // 3. Verify title:
         //Expected: “Facebook - Log In or Sign Up”
+        String expectedTitle = "Facebook - Log In or Sign Up";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("Title verification passed!");
+        }else{
+
+            System.out.println("Title verification failed!!!");
+        }
+
+
 
     }
 
