839d888b14bb9c8b359d85eee54ed845949f6589
gurhank <gurhankocyigit@gmail.com>
2021-05-12 20:35:10 -0500

    day 3 Facebook task 3

--- src/test/java/com/cybertek/tests/day3_reviews_practices/F3_HeaderVerification.java
+++ src/test/java/com/cybertek/tests/day3_reviews_practices/F3_HeaderVerification.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day3_reviews_practices;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class F3_HeaderVerification {
@@ -17,7 +19,16 @@ public class F3_HeaderVerification {
         driver.get("https://www.facebook.com ");
 
         //3. Verify header text is as expected:
+        WebElement header = driver.findElement(By.className("_8eso"));
+
+        // short cut for auto-generating variable type
+        // alt + ENTER --> WINDOWS
+        // option + ENTER --> MAC
+
         //Expected: “Connect with friends and the world around you on Facebook.”
+        String expectedHeader = "Connect with friends and the world around you on Facebook.";
+        String actualHeader = header.getText();
+
 
     }
 
