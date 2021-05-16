7e89aeb13164e252f8cb6645fae5707338ae4110
gurhank <gurhankocyigit@gmail.com>
2021-05-16 15:52:01 -0500

    day 4 xpath task1

--- src/test/java/com/cybertek/tests/day4_cssSelector_xpath/P6_xpath_MultipleButtons.java
+++ src/test/java/com/cybertek/tests/day4_cssSelector_xpath/P6_xpath_MultipleButtons.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day4_cssSelector_xpath;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class P6_xpath_MultipleButtons {
 
@@ -16,6 +18,19 @@ public class P6_xpath_MultipleButtons {
         driver.get("http://practice.cybertekschool.com/multiple_buttons");
 
         // 3. Click on Button 1
+        // locating using XPATH with class attribute value
+        //WebElement button1= driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
+
+        // locating using text of the button with XPATH
+        //WebElement button1= driver.findElement(By.xpath("//button[.='Button 1']"));
+
+        // locating using "onclick" attribute value with xpath locator
+        WebElement button1= driver.findElement(By.xpath("//button[@onclick='button1()']"));
+
+        button1.click();
+
+        WebElement resultText = driver.findElement(By.xpath(""));
+
         // 4. Verify text displayed is as expected:
         // Expected: “Clicked on button one!”
 
