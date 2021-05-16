32339933b08f9e1e7b4e73e2d7eb0025430e1c59
gurhank <gurhankocyigit@gmail.com>
2021-05-16 11:11:14 -0500

    day 4 UTILITY CLASS

--- src/test/java/com/cybertek/utilities/WebDriverFactory.java
+++ src/test/java/com/cybertek/utilities/WebDriverFactory.java
@@ -9,7 +9,35 @@ package com.cybertek.utilities;
 //   - if "firefox" passed --> it will open firefox browser
 // RETURN TYPE: "WebDriver"
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class WebDriverFactory {
+
+    public static WebDriver getDriver(String browserType){
+
+        if (browserType.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+
+            return new ChromeDriver();
+
+        }else if (browserType.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+
+            return new FirefoxDriver();
+
+        }else{
+            System.out.println("Given browser type does not exist.  Driver = null");
+            return null;
+        }
+
+    }
+
+
+
+
 
 
 
