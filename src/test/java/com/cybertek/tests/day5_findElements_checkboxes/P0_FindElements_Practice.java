78b2995a2128cb16a47b989659529f4f87599ba1
gurhank <gurhankocyigit@gmail.com>
2021-05-19 20:32:59 -0500

    day 5 find elements method practice

--- src/test/java/com/cybertek/tests/day5_findElements_checkboxes/P0_FindElements_Practice.java
+++ src/test/java/com/cybertek/tests/day5_findElements_checkboxes/P0_FindElements_Practice.java
@@ -20,7 +20,7 @@ public class P0_FindElements_Practice {
 
 
         //2. Go to http://practice.cybertekschool.com/forgot_password
-
+        driver.get("http://practice.cybertekschool.com/forgot_password");
 
         //3. Print out the texts of all links
         //4. Print out how many total link
