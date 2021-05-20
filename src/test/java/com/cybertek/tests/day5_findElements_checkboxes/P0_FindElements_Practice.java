64f0fc812a558d283c1911dcca02918157f494f4
gurhank <gurhankocyigit@gmail.com>
2021-05-19 20:28:17 -0500

    day 5 find elements method practice

---
+++ src/test/java/com/cybertek/tests/day5_findElements_checkboxes/P0_FindElements_Practice.java
@@ -0,0 +1,12 @@
+package com.cybertek.tests.day5_findElements_checkboxes;
+
+public class P0_FindElements_Practice {
+
+    public static void main(String[] args) {
+        //TC #0: FINDELEMENTS
+        //1. Open Chrome browser
+        //2. Go to http://practice.cybertekschool.com/forgot_password
+        //3. Print out the texts of all links
+        //4. Print out how many total link
+    }
+}
