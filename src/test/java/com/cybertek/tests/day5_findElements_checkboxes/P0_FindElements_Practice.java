6caf2dd3b12ec71a1ac09ec845098b82fa0f495e
gurhank <gurhankocyigit@gmail.com>
2021-05-19 20:30:43 -0500

    day 5 find elements method practice

--- src/test/java/com/cybertek/tests/day5_findElements_checkboxes/P0_FindElements_Practice.java
+++ src/test/java/com/cybertek/tests/day5_findElements_checkboxes/P0_FindElements_Practice.java
@@ -1,11 +1,27 @@
 package com.cybertek.tests.day5_findElements_checkboxes;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class P0_FindElements_Practice {
 
     public static void main(String[] args) {
         //TC #0: FINDELEMENTS
         //1. Open Chrome browser
+
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        // If a web element is not found, this will wait UP TO given seconds
+        // If web element is located before 15 seconds, it will move on
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+
         //2. Go to http://practice.cybertekschool.com/forgot_password
+
+
         //3. Print out the texts of all links
         //4. Print out how many total link
     }
