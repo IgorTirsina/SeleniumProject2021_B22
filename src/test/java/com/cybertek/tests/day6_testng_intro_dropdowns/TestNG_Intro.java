f50c697ee076f057b6f4b32413096fe27c02bf6f
gurhank <gurhankocyigit@gmail.com>
2021-05-23 14:36:01 -0500

    day 6 - TestNG_Intro

--- src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/TestNG_Intro.java
@@ -1,10 +1,18 @@
 package com.cybertek.tests.day6_testng_intro_dropdowns;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
+
+    @BeforeClass
+    public void setupClass(){
+        System.out.println("-----> Before CLASS is running...");
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("-----> After CLASS is running...");
+    }
 
     @BeforeMethod
     public void setupMethod(){
