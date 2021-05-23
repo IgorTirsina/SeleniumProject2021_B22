83d756e0ffedc4559f7951545ce674dc44f90ab2
gurhank <gurhankocyigit@gmail.com>
2021-05-23 12:01:19 -0500

    day 6 apple p03 practice

--- src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/P03_Apple_task.java
+++ src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/P03_Apple_task.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class P03_Apple_task {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //TC #03: FINDELEMENTS_APPLE
         //1. Open Chrome browser
@@ -40,13 +40,20 @@ public class P03_Apple_task {
             //System.out.println(headerLinks.get(eachLink).getText());
 
             headerLinks.get(eachLink).click();
-
+            Thread.sleep(1000);
             headerLinks = driver.findElements(By.xpath("//ul[@class='ac-gn-list']//li/a"));
+
+            //4. Print out the titles of the each page
+            System.out.println("Current title in the page:" + driver.getTitle());
+
+            //5. Print out total number of links in each page
+            List<WebElement> allLinks = driver.findElements(By.xpath("//body//a"));
+
+            System.out.println("Number of links in current page: " + allLinks.size());
+
 
         }
 
-        //4. Print out the titles of the each page
-        //5. Print out total number of links in each page
         //6. While in each page:
             // a. Print out how many link is missing text TOTAL
             // b. Print out how many link has text TOTAL
