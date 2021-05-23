abe916dc81ef752d46ce2cbf266463a872ecbf76
gurhank <gurhankocyigit@gmail.com>
2021-05-23 14:26:23 -0500

    day 6 - TestNG_Intro

--- src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/TestNG_Intro.java
@@ -1,8 +1,14 @@
 package com.cybertek.tests.day6_testng_intro_dropdowns;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_Intro {
+
+    @BeforeMethod
+    public void setupMethod(){
+        System.out.println("---> Before method is running...");
+    }
 
     @Test
     public void test1(){
@@ -12,7 +18,10 @@ public class TestNG_Intro {
     @Test
     public void test2(){
         System.out.println("Running test 2...");
+
     }
+
+    //BREAK UNTIL 2.15PM CST
 
 
 
