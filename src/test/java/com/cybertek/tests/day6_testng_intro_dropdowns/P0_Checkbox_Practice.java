134b2314bb730d39e15aa64c3656fbbc3976b943
gurhank <gurhankocyigit@gmail.com>
2021-05-23 10:29:29 -0500

    day 6 checkbox practice

--- src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/P0_Checkbox_Practice.java
+++ src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/P0_Checkbox_Practice.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day6_testng_intro_dropdowns;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,9 +17,14 @@ public class P0_Checkbox_Practice {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         //1. Go to http://practice.cybertekschool.com/checkboxes
-        driver.get("http://practice.cybertekschool.com/checkboxes ");
+        driver.get("http://practice.cybertekschool.com/checkboxes");
+
+        WebElement checkbox1 = driver.findElement(By.xpath("(//input[@type='checkbox'])[1]"));
+
         //2. Confirm checkbox #1 is NOT selected by default
         //3. Confirm checkbox #2 is SELECTED by default.
+
+
         //4. Click checkbox #1 to select it.
         //5. Click checkbox #2 to deselect it.
         //6. Confirm checkbox #1 is SELECTED.
