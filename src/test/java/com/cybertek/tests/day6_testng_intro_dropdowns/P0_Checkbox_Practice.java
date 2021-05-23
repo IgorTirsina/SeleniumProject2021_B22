adb26448e8954553d913e387bf51576d7f14aaff
gurhank <gurhankocyigit@gmail.com>
2021-05-23 10:18:36 -0500

    day 6 checkbox practice

---
+++ src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/P0_Checkbox_Practice.java
@@ -0,0 +1,27 @@
+package com.cybertek.tests.day6_testng_intro_dropdowns;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class P0_Checkbox_Practice {
+
+    public static void main(String[] args) {
+
+        //Practice: Cybertek Checkboxes
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //1. Go to http://practice.cybertekschool.com/checkboxes
+        driver.get("http://practice.cybertekschool.com/checkboxes ");
+        //2. Confirm checkbox #1 is NOT selected by default
+        //3. Confirm checkbox #2 is SELECTED by default.
+        //4. Click checkbox #1 to select it.
+        //5. Click checkbox #2 to deselect it.
+        //6. Confirm checkbox #1 is SELECTED.
+        //7. Confirm checkbox #2 is NOT selected.
+
+    }
+}
