104d004afa5d7bc9d3194c64347bcad58320c783
gurhank <gurhankocyigit@gmail.com>
2021-05-30 15:45:27 -0500

    day 7 - window practices 1

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/WindowPractice.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/WindowPractice.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day7_alerts_iframes_windows;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -27,7 +29,10 @@ public class WindowPractice {
         String actualTitleBeforeClick = driver.getTitle();
         String expectedTitleBeforeClick = "Practice";
 
+        System.out.println("actualTitleBeforeClick = " + actualTitleBeforeClick);
+
         //5. Click to: “Click Here” text
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
         //6. Switch to new Window.
         //7. Assert: Title is “New Window”
     }
