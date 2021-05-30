f0fa4f0e6dd4c2a9864e500a5c827c708242b822
gurhank <gurhankocyigit@gmail.com>
2021-05-30 14:47:32 -0500

    day 7 - IframePractice practices 1

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/IframePractice.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/IframePractice.java
@@ -4,6 +4,7 @@ import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -31,12 +32,22 @@ public class IframePractice {
 
         //#2- Locate as a Web Element to switch
         // We find and locate <iframe> web element from the page.
-        WebElement iframe = driver.findElement(By.xpath("//iframe[@class='tox-edit-area__iframe']"));
+        //WebElement iframe = driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']"));
+        //WebElement iframe = driver.findElement(By.xpath("//iframe[@title='Rich Text Area. Press ALT-0 for help.']"));
+        //WebElement iframe = driver.findElement(By.xpath("//iframe[@class='tox-edit-area__iframe']"));
+        //driver.switchTo().frame(iframe);
+
+        //#3- If <iframe> tag has id or name attribute, we can pass its attribute value as a string into
+        // .switchTo().frame() method directly
+        driver.switchTo().frame("mce_0_ifr");
+
 
         //locating web element text
         WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
 
         //4. Assert: “Your content goes here.” Text is displayed.
+        Assert.assertTrue(yourContentGoesHereText.isDisplayed());
+
 
         //5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor
 
