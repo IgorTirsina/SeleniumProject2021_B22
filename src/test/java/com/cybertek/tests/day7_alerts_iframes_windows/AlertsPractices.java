7929a8516ca4cfc1a48bae9e8e4b7edfe7c89f07
gurhank <gurhankocyigit@gmail.com>
2021-05-30 13:19:42 -0500

    day 7 - Alerts practices

---
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/AlertsPractices.java
@@ -0,0 +1,24 @@
+package com.cybertek.tests.day7_alerts_iframes_windows;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class AlertsPractices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/dropdown");
+
+
+    }
+
+}
