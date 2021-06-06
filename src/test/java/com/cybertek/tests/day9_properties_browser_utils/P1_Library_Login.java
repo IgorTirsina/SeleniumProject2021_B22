9ed7d40612564dd47ccea30e4848a41a5867ac9d
gurhank <gurhankocyigit@gmail.com>
2021-06-06 13:39:08 -0500

    day 9 - P1_Library task1

--- src/test/java/com/cybertek/tests/day9_properties_browser_utils/P1_Library_Login.java
+++ src/test/java/com/cybertek/tests/day9_properties_browser_utils/P1_Library_Login.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day9_properties_browser_utils;
 
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -24,11 +25,7 @@ public class P1_Library_Login {
 
     }
 
-    @Test
-    public void login_link_count_test(){
-        //Task #1: Library software link verification
-
-        //3. Enter username: “”
+    public void loginToLibrary(){
         WebElement inputUsername = driver.findElement(By.xpath("//input[@id='inputEmail']"));
         inputUsername.sendKeys("student11@library");
 
@@ -39,9 +36,19 @@ public class P1_Library_Login {
         //5. Click to Login button
         WebElement loginButton = driver.findElement(By.xpath("//button[text()='Sign in']"));
         loginButton.click();
+    }
+
+    @Test
+    public void login_link_count_test(){
+        //Task #1: Library software link verification
+        //loginMethod calling to login Library app
+        loginToLibrary();
 
 
         //6. Print out count of all the links on landing page
+
+        BrowserUtils.sleep(2);
+
         List<WebElement> allLinks = driver.findElements(By.xpath("//body//a"));
 
         System.out.println("allLinks.size() = " + allLinks.size());
