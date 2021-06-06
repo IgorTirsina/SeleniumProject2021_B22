a750dbe26e564983d0496a268eeb1db9f591a23c
gurhank <gurhankocyigit@gmail.com>
2021-06-06 15:42:54 -0500

    day 9 - ConfigurationReader

---
+++ src/test/java/com/cybertek/utilities/ConfigurationReader.java
@@ -0,0 +1,13 @@
+package com.cybertek.utilities;
+
+import java.util.Properties;
+
+/*
+We will create a re-usable method that will be reading from configuration.reader file
+ */
+
+public class ConfigurationReader {
+
+    Properties properties = new Properties();
+
+}
