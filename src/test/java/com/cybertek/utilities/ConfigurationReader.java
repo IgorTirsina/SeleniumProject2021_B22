f14ce3c4622b40820da5e3b32a49468ebb7edfd5
gurhank <gurhankocyigit@gmail.com>
2021-06-06 15:50:55 -0500

    day 9 - ConfigurationReader

--- src/test/java/com/cybertek/utilities/ConfigurationReader.java
+++ src/test/java/com/cybertek/utilities/ConfigurationReader.java
@@ -1,5 +1,7 @@
 package com.cybertek.utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Properties;
 
 /*
@@ -8,6 +10,22 @@ We will create a re-usable method that will be reading from configuration.reader
 
 public class ConfigurationReader {
 
-    Properties properties = new Properties();
+    //#1- Create properties object
+    private static Properties properties = new Properties();
+
+    static {
+
+        try {
+            //#2- Load the file into FileInputStream
+            FileInputStream file = new FileInputStream("configuration.properties");
+
+            //#3- load properties object with the file (configuration.properties)
+            properties.load(file);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
