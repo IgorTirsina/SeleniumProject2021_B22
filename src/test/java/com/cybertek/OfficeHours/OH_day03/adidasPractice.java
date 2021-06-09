e3f8019f0822dc018e82087dd73e37fe59f015cc
OnderSahan <osahan77@gmail.com>
2021-06-10 01:14:11 +0300

    OH Day3  Commit

--- src/test/java/com/cybertek/OfficeHours/OH_day03/adidasPractice.java
+++ src/test/java/com/cybertek/OfficeHours/OH_day03/adidasPractice.java
@@ -1,6 +1,7 @@
 package com.cybertek.OfficeHours.OH_day03;
 
 import com.cybertek.utilities.BrowserUtils;
+import com.cybertek.utilities.OfficeHoursUtils;
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -21,24 +22,16 @@ public class adidasPractice {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.demoblaze.com/index.html");
     }
-    public void productAdder(String category, String product){
-        driver.findElement(By.xpath("//a[.='"+category+"']")).click();
-        BrowserUtils.sleep(1);
-        driver.findElement(By.xpath("//a[.='"+product+"']")).click();
-        BrowserUtils.sleep(1);
-        driver.findElement(By.xpath("//a[.='Add to cart']")).click();
-        BrowserUtils.sleep(1);
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
-        BrowserUtils.sleep(1);
-        driver.findElement(By.xpath("(//a[@class='nav-link'])[1]")).click();
-    }
+
 
     @Test
     public void adidasTest(){
         // Navigate to "Laptop" → "Sony vaio i5" and click on "Add to cart".
-        productAdder("Laptops","Sony vaio i5");
-
+        OfficeHoursUtils.productAdder(driver,"Laptops","Sony vaio i5");
+        // Navigate to "Phones" → "Samsung galaxy s6" and click on "Add to cart".
+        OfficeHoursUtils.productAdder(driver,"Phones","Samsung galaxy s6");
+        // Navigate to "Monitors" → "Apple monitor 24" and click on "Add to cart".
+        OfficeHoursUtils.productAdder(driver,"Monitors","Apple monitor 24");
     }
 
     @AfterMethod
