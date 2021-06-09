e01090c832065d35c93726baa6f8eb6fd0ee212c
OnderSahan <osahan77@gmail.com>
2021-06-10 02:30:07 +0300

    Latest version of Adidas Task Office Hours Day3

--- src/test/java/com/cybertek/OfficeHours/OH_day03/adidasPractice.java
+++ src/test/java/com/cybertek/OfficeHours/OH_day03/adidasPractice.java
@@ -17,10 +17,11 @@ import java.util.concurrent.TimeUnit;
 public class adidasPractice {
 
     WebDriver driver;
-
+    int expectedOrderAmmount = 0;
     @BeforeMethod
     public void setup(){
         driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.demoblaze.com/index.html");
     }
@@ -28,15 +29,15 @@ public class adidasPractice {
 
     @Test
     public void adidasTest(){
-        // Navigate to "Laptop" → "Sony vaio i5" and click on "Add to cart".
-        OfficeHoursUtils.productAdder(driver,"Laptops","Sony vaio i5");
-        // Navigate to "Phones" → "Samsung galaxy s6" and click on "Add to cart".
-        OfficeHoursUtils.productAdder(driver,"Phones","Samsung galaxy s6");
-        // Navigate to "Monitors" → "Apple monitor 24" and click on "Add to cart".
-        OfficeHoursUtils.productAdder(driver,"Monitors","Apple monitor 24");
+        // Navigate to "Laptop" → "Sony vaio i5" and click on "Add to cart" and adding the price to order amount
+        expectedOrderAmmount+=OfficeHoursUtils.productAdder(driver,"Laptops","Sony vaio i5");
+        // Navigate to "Phones" → "Samsung galaxy s6" and click on "Add to cart" and adding the price to order amount
+        expectedOrderAmmount+=OfficeHoursUtils.productAdder(driver,"Phones","Samsung galaxy s6");
+        // Navigate to "Monitors" → "Apple monitor 24" and click on "Add to cart" and adding the price to order amount
+        expectedOrderAmmount+=OfficeHoursUtils.productAdder(driver,"Monitors","Apple monitor 24");
 
         // Remove  Apple monitor 24
-        OfficeHoursUtils.productRemover(driver,"Apple monitor 24");
+        expectedOrderAmmount-=OfficeHoursUtils.productRemover(driver,"Apple monitor 24");
 
         driver.findElement(By.xpath("//button[.='Place Order']")).click();
 
@@ -57,9 +58,9 @@ public class adidasPractice {
         System.out.println("orderID = " + orderID);
         int actualAmmount = Integer.parseInt(confirmationArray[1].split(" ")[1]);
         System.out.println("actualAmmount = " + actualAmmount);
-        int expectedAmount = 1150;
+        System.out.println("expectedOrderAmmount = " + expectedOrderAmmount);
 
-        Assert.assertEquals(actualAmmount,expectedAmount);
+        Assert.assertEquals(actualAmmount,expectedOrderAmmount);
     }
 
     @AfterMethod
