9c0e1de2715eb1fc12a9ca7df1430a99942f924f
gurhank <gurhankocyigit@gmail.com>
2021-06-09 20:20:04 -0500

    day 10 - webtable task 3

--- src/test/java/com/cybertek/tests/day10_webtable_properties_practices/Table_Tasks.java
+++ src/test/java/com/cybertek/tests/day10_webtable_properties_practices/Table_Tasks.java
@@ -5,6 +5,7 @@ import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -44,6 +45,14 @@ public class Table_Tasks {
         // 2nd way: //table[@id='table1']//td[.='Tim']/following-sibling::td[2]
 
         WebElement timsDueAmountCell = driver.findElement(By.xpath("//table[@id='table1']//td[.='Tim']/../td[4]"));
+
+        System.out.println("timsDueAmountCell = " + timsDueAmountCell.getText());
+
+        String actualTimResult = timsDueAmountCell.getText();
+        String expectedTimResult = "$50.00";
+
+        Assert.assertEquals(actualTimResult, expectedTimResult, "Tim's cell is not returning as expected.");
+
 
 
         //Note: Create locator for Tim that will be dynamic and doesn’t care in which row Tim is.
