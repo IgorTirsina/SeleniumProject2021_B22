9997f83122f202ca1bc6bbd6d3c534260ee70ba7
gurhank <gurhankocyigit@gmail.com>
2021-06-09 20:30:16 -0500

    day 10 - webtable task 4

---
+++ src/test/java/com/cybertek/utilities/TableUtils.java
@@ -0,0 +1,18 @@
+package com.cybertek.utilities;
+
+public class TableUtils {
+
+    //PRACTICE #4: Method: verifyOrder
+    //• Create a method named verifyOrder in TableUtils class.
+    //• Method takes WebDriver object and String(name).
+    //• Method should verify if given name exists in orders.
+    //• This method should simply accepts a name(String), and assert whether
+    //given name is in the list or not.
+    //• Create a new TestNG test to test if the method is working as expected.
+
+    public static void verifyOrder(){
+
+    }
+
+
+}
