e21ce11c6b17b598570e44de1b885d9c268ed77d
gurhank <gurhankocyigit@gmail.com>
2021-06-09 19:19:30 -0500

    day 10 - webtable task 3

--- src/test/java/com/cybertek/tests/day10_webtable_properties_practices/Table_Tasks.java
+++ src/test/java/com/cybertek/tests/day10_webtable_properties_practices/Table_Tasks.java
@@ -1,8 +1,26 @@
 package com.cybertek.tests.day10_webtable_properties_practices;
 
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class Table_Tasks {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+
+        driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty(""));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("");
+
+    }
 
     @Test
     public void task3_return_tims_due_amount(){
