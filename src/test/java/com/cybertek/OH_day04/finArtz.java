2bb63e513b16fd893d1044afe65e85cd0adbf4d4
OnderSahan <osahan77@gmail.com>
2021-06-17 01:49:54 +0300

    Office Hours Day4

--- src/test/java/com/cybertek/OH_day04/finArtz.java
+++ src/test/java/com/cybertek/OH_day04/finArtz.java
@@ -4,6 +4,7 @@ import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.OfficeHoursUtils;
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -37,11 +38,29 @@ public class finArtz {
         // Negative Login test
 
         Assert.assertTrue(OfficeHoursUtils.negativeLoginTest(driver,loginUrl));
+  }
+
+  @Test
+  public void textTests(){
+        driver.get(loginUrl);
+        String expectedTab = "Login example page to test the PHP MySQL online system";
+        String actualTab = driver.getTitle();
+        Assert.assertEquals(actualTab,expectedTab);
+
+        String actualHeader = driver.findElement(By.xpath("(//strong)[3]")).getText();
+        String expectedHeader = "4. Login";
+        Assert.assertEquals(actualHeader,expectedHeader);
+
+        String expectedFuunyText = "Enter your login details you added on the previous page and test the login.\n" +
+              "The success or failure will be shown above.";
+        String actualText = driver.findElement(By.xpath("//p[contains(.,'login details you added')]")).getText();
+        Assert.assertEquals(actualText,expectedFuunyText);
 
 
 
 
-    }
+
+  }
 
     @AfterMethod
     public void tearDown(){
