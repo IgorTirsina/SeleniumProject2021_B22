062aa04b95560357aaee08b648b40bbc1fc108d1
gurhank <gurhankocyigit@gmail.com>
2021-06-16 20:28:37 -0500

    day 11 - driver utils

---
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -0,0 +1,11 @@
+package com.cybertek.utilities;
+
+public class Driver {
+
+    /*
+    Creating the private constructor so this class' object
+    is not reachable from outside
+     */
+    private Driver(){}
+
+}
