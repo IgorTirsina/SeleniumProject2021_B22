d8c7ed5d2cfc0cb759c25a392dea0214295f829d
gurhank <gurhankocyigit@gmail.com>
2021-06-19 11:04:38 -0500

    day 12 - Singleton practice

---
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/Singleton.java
@@ -0,0 +1,18 @@
+package com.cybertek.tests.day12_actions_upload_jsexecutor;
+
+/*
+    We will create similar logic we created in Driver utility class.
+    But it will be for a String.
+*/
+
+public class Singleton {
+
+    //Create private constructor
+    private Singleton(){}
+
+    //By making the String word private,
+    // we will ensure we create access only through our getter method
+    private static String word;
+
+
+}
