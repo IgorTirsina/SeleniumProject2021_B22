1be30889771d9d478a2a7d4ce0e738cbdf627901
gurhank <gurhankocyigit@gmail.com>
2021-06-19 10:09:37 -0500

    day 12 - Driver utility practice

--- src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/DriverUtilPractice.java
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/DriverUtilPractice.java
@@ -2,6 +2,8 @@ package com.cybertek.tests.day12_actions_upload_jsexecutor;
 
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class DriverUtilPractice {
@@ -13,8 +15,12 @@ public class DriverUtilPractice {
         //Driver.getDriver() = driver --> creates driver instance and ready to use.
         Driver.getDriver().get(ConfigurationReader.getProperty("googleUrl"));
 
-
         //2- Search for a value
+        WebElement searchBox = Driver.getDriver().findElement(By.name("q"));
+
+        String searchValue = "there is no spoon";
+
+
         //3- Verify value is contained in the title
 
     }
