3d1f77367f25460d6e3ee382cd9af3981911fc59
gurhank <gurhankocyigit@gmail.com>
2021-06-19 11:16:00 -0500

    day 12 - Singleton practice

--- src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/SingletonPractice.java
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/SingletonPractice.java
@@ -10,6 +10,13 @@ public class SingletonPractice {
         String str1= Singleton.getWord();
         System.out.println("str1 = " + str1);
 
+        String str2= Singleton.getWord();
+        System.out.println("str2 = " + str2);
+
+        String str3= Singleton.getWord();
+        System.out.println("str3 = " + str3);
+
+
 
     }
 
