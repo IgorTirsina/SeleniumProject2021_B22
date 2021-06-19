e09d6d899b1ab4953c9fc4d24be361bbcb911a0a
gurhank <gurhankocyigit@gmail.com>
2021-06-19 15:44:36 -0500

    day 12 - JavaScriptExecutor practice

--- src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/JSExecutorPractices.java
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/JSExecutorPractices.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day12_actions_upload_jsexecutor;
 
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
@@ -14,13 +15,19 @@ public class JSExecutorPractices {
         Driver.getDriver().get("http://practice.cybertekschool.com/infinite_scroll");
 
         //((JavascriptExecutor) Driver.getDriver()).executeScript("")
-
+        BrowserUtils.sleep(2);
         // Down casting our driver type to JavascriptExecutor so we can reach methods in this interface
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
         //using js we reach executeScript method so we are allowed to pass JavaScript functions.
         //this method is capable of accepting JavaScript functions and apply that into our Java+Selenium code
-        js.executeScript("window.scrollBy(0, 750)");
+
+        for (int i = 0; i < 10; i++) {
+
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0, 750)");
+
+        }
 
 
 
