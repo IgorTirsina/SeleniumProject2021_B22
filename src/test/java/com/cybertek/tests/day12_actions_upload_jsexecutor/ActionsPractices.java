e0f5d84df50a8b2b64cba2601a48a18b661b72fa
gurhank <gurhankocyigit@gmail.com>
2021-06-19 13:39:29 -0500

    day 12 - Upload practice

---
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/ActionsPractices.java
@@ -0,0 +1,23 @@
+package com.cybertek.tests.day12_actions_upload_jsexecutor;
+
+import org.testng.annotations.Test;
+
+public class ActionsPractices {
+
+    @Test
+    public void tc15_actions_hover(){
+
+        //TC #15: Hover Test
+        //1. Go to http://practice.cybertekschool.com/hovers
+        //2. Hover over to first image
+        //3. Assert:
+        //a. “name: user1” is displayed 4. Hover over to second image
+        //5. Assert:
+        //a. “name: user2” is displayed 6. Hover over to third image
+        //7. Confirm:
+        //a. “name: user3” is displayed
+
+
+    }
+
+}
