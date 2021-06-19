9b32814d7adb3044aaa8ff0db15472a00035b2ce
gurhank <gurhankocyigit@gmail.com>
2021-06-19 10:04:47 -0500

    day 12 - Driver utility practice

--- src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/DriverUtilPractice.java
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/DriverUtilPractice.java
@@ -1,5 +1,7 @@
 package com.cybertek.tests.day12_actions_upload_jsexecutor;
 
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
 import org.testng.annotations.Test;
 
 public class DriverUtilPractice {
@@ -8,6 +10,10 @@ public class DriverUtilPractice {
     public void simple_google_search_test(){
 
         //1- Go to google.com
+        //Driver.getDriver() = driver --> creates driver instance and ready to use.
+        Driver.getDriver().get(ConfigurationReader.getProperty("googleUrl"));
+
+
         //2- Search for a value
         //3- Verify value is contained in the title
 
