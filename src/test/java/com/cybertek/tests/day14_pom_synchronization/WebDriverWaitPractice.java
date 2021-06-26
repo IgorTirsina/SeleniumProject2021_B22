41ea73d223f9c74bdfd751f5d2bea6a07e96c3c7
gurhank <gurhankocyigit@gmail.com>
2021-06-26 11:44:01 -0500

    day 14 - webdriver wait practice

---
+++ src/test/java/com/cybertek/tests/day14_pom_synchronization/WebDriverWaitPractice.java
@@ -0,0 +1,17 @@
+package com.cybertek.tests.day14_pom_synchronization;
+
+import org.testng.annotations.Test;
+
+public class WebDriverWaitPractice {
+
+    @Test
+    public void dynamic_loading_7_test(){
+
+//        1. Go to http://practice.cybertekschool.com/dynamic_loading/7
+//        2. Wait until title is “Dynamic title”
+//        3. Assert : Message “Done” is displayed.
+//        4. Assert : Image is displayed.
+
+
+    }
+}
