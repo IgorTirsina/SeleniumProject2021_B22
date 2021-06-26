f2a26f3ef617fa787c409521351150dad6d6784c
gurhank <gurhankocyigit@gmail.com>
2021-06-26 13:19:10 -0500

    day 14 - webdriver wait practice 2

--- src/test/java/com/cybertek/pages/DynamicLoad1Page.java
+++ src/test/java/com/cybertek/pages/DynamicLoad1Page.java
@@ -1,6 +1,8 @@
 package com.cybertek.pages;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DynamicLoad1Page {
@@ -8,6 +10,9 @@ public class DynamicLoad1Page {
     public DynamicLoad1Page(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//button[.='Start']")
+    public WebElement startButton;
 
 
 
